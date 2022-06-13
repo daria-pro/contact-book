af42930eec51db08bd42c71f08e439d0fe0fdee8
Daria <dshaulko@kaaiot.io>
2022-06-13 16:12:05 +0300

    fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { ToastBody } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import About from "./components/About/About";
 import Contacts from "./components/Contacts/Contacts";
-import EditContactForm from "./components/EditContactForm/EditContactForm";
 import Home from "./components/Home/Home";
 import Navigation from "./components/Navigation/Navigation";
 import NotFound from "./components/NotFound/NotFound";
@@ -12,12 +9,11 @@ const App: React.FC = () => {
   return (
     <div className="app-wrapper">
       <Navigation />
-      <ToastContainer />
       <Routes>
+        <Route path="/contact-book" element={<Home />} />
+        <Route path="/contact-book/contacts" element={<Contacts />} />
+        <Route path="/contact-book/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
--- src/dist/App.js
+++ src/dist/App.js
@@ -1,7 +1,6 @@
 "use strict";
 exports.__esModule = true;
 var react_router_dom_1 = require("react-router-dom");
-var react_toastify_1 = require("react-toastify");
 var About_1 = require("./components/About/About");
 var Contacts_1 = require("./components/Contacts/Contacts");
 var Home_1 = require("./components/Home/Home");
@@ -10,11 +9,10 @@ var NotFound_1 = require("./components/NotFound/NotFound");
 var App = function () {
     return (React.createElement("div", { className: "app-wrapper" },
         React.createElement(Navigation_1["default"], null),
-        React.createElement(react_toastify_1.ToastContainer, null),
         React.createElement(react_router_dom_1.Routes, null,
-            React.createElement(react_router_dom_1.Route, { path: "*", element: React.createElement(NotFound_1["default"], null) }),
-            React.createElement(react_router_dom_1.Route, { path: "/", element: React.createElement(Home_1["default"], null) }),
-            React.createElement(react_router_dom_1.Route, { path: "/contacts", element: React.createElement(Contacts_1["default"], null) }),
-            React.createElement(react_router_dom_1.Route, { path: "/about", element: React.createElement(About_1["default"], null) }))));
+            React.createElement(react_router_dom_1.Route, { path: "/contact-book", element: React.createElement(Home_1["default"], null) }),
+            React.createElement(react_router_dom_1.Route, { path: "/contact-book/contacts", element: React.createElement(Contacts_1["default"], null) }),
+            React.createElement(react_router_dom_1.Route, { path: "/contact-book/about", element: React.createElement(About_1["default"], null) }),
+            React.createElement(react_router_dom_1.Route, { path: "*", element: React.createElement(NotFound_1["default"], null) }))));
 };
 exports["default"] = App;
